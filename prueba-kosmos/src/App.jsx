4d3e2fbede830b431fb82d85f09bb8920e2fcace
Aaron <aaron_ym2892@hotmail.com>
2022-11-08 14:03:33 -0600

    First commit

---
+++ prueba-kosmos/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { TestContainer } from './components/TestContainer'
+
+function App() {
+
+  return (
+    <div className="App">
+      <TestContainer/>
+    </div>
+  )
+}
+
+export default App
---
+++ prueba-kosmos/src/components/TestContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TestContainer = () => {
+  return (
+    <div>TestContainer</div>
+  )
+}
