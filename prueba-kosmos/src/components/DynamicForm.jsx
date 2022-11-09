3fa53916d955563f4035bc133bc089beed7b4336
Aaron <aaron_ym2892@hotmail.com>
2022-11-08 18:07:47 -0600

    Conditional rendering and styles

--- prueba-kosmos/src/components/DynamicForm.jsx
+++ prueba-kosmos/src/components/DynamicForm.jsx
@@ -20,17 +20,34 @@ export const DynamicForm = () => {
         </div>
         <div className='form-elements'>
             <div>
-            <form className='form-style' action="">
-                {
-                    formFields.map( (field, index) => {
-                     return (
-                        <>
-                        <input type={field.type} label={field.label} placeholder={field.label}/>
-                        <button type="button" onClick={() => removeFields(index)}>Borrar</button>
-                        </>
-                     )
-                    })
-                }
+            <form action="">
+                {formFields.map( (field, index) => {
+                    return (
+                        <div className='test-container'>
+                            {   field.type === "text" &&
+                                    <div>
+                                        <input type={field.type} label={field.label} placeholder={field.label}/>
+                                        <button type="button" onClick={() => removeFields(index)}>Borrar</button> 
+                                    </div>
+                            }
+
+                            {   field.type === "radio" && 
+                                    <div>
+                                        <input type="radio" id={field._uid} name={field.label} value={field.label}/>
+                                        <label for={field.uid}>{field.label}</label>
+                                        <button type="button" onClick={() => removeFields(index)}>Borrar</button>
+                                    </div>
+                            }
+                            {   field.type === "checkbox" && 
+                                    <div>
+                                        <input type="checkbox" id={field._uid} name={field.label} value={field.label}/>
+                                        <label for={field.uid}>{field.label}</label>
+                                        <button type="button" onClick={() => removeFields(index)}>Borrar</button>
+                                    </div>
+                            }
+                        </div>
+                    )
+                })}
             </form>
             </div>
         </div>
